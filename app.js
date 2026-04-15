c505f4c15d6b54f7a6602403da5b98a3c835cfc4
d1000m3000-sys <d1000m3000@gmail.com>
2026-04-15 15:10:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,15 @@
 const stories = [
-  {
-    img: "https://picsum.photos/400/600?1",
-    title: "الحب الحقيقي"
-  },
-  {
-    img: "https://picsum.photos/400/600?2",
-    title: "الحياة صعبة لكن جميلة"
-  },
-  {
-    img: "https://picsum.photos/400/600?3",
-    title: "لا تستسلم أبداً"
-  },
-  {
-    img: "https://picsum.photos/400/600?4",
-    title: "الله مع الصابرين"
-  }
+  { img: "https://picsum.photos/300/400?1", title: "قريباً حالات" },
+  { img: "https://picsum.photos/300/400?2", title: "Story+" }
 ];
 
-const grid = document.getElementById("grid");
-const viewer = document.getElementById("viewer");
-const viewerImg = document.getElementById("viewerImg");
+const grid = document.getElementById("stories");
 
-let currentImg = "";
-
-stories.forEach(story => {
+stories.forEach(s => {
   grid.innerHTML += `
-    <div class="card" onclick="openViewer('${story.img}')">
-      <img src="${story.img}" />
-      <p>${story.title}</p>
+    <div class="card">
+      <img src="${s.img}">
+      <p>${s.title}</p>
     </div>
   `;
 });
-
-function openViewer(img) {
-  currentImg = img;
-  viewerImg.src = img;
-  viewer.style.display = "flex";
-}
-
-function closeViewer() {
-  viewer.style.display = "none";
-}
-
-function downloadImg() {
-  const a = document.createElement("a");
-  a.href = currentImg;
-  a.download = "story.png";
-  a.click();
-}
-
-async function shareImg() {
-  if (navigator.share) {
-    await navigator.share({
-      title: "Story+",
-      url: currentImg
-    });
-  } else {
-    alert("المشاركة غير مدعومة في هذا الجهاز");
-  }
-}
