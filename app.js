f8ea40b59bc63a0b22685416d77f23aa1f3ca49d
d1000m3000-sys <d1000m3000@gmail.com>
2026-04-15 21:33:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const stories = [
-  { img: "https://picsum.photos/300/400?1", title: "قريباً حالات" },
-  { img: "https://picsum.photos/300/400?2", title: "Story+" }
-];
-
 const grid = document.getElementById("stories");
 
-stories.forEach(s => {
-  grid.innerHTML += `
-    <div class="card">
-      <img src="${s.img}">
-      <p>${s.title}</p>
-    </div>
-  `;
-});
+async function loadStories() {
+  const res = await fetch("stories.json");
+  const data = await res.json();
+
+  data.forEach(story => {
+    grid.innerHTML += `
+      <div class="card">
+        <img src="${story.image}">
+        <p>${story.title}</p>
+      </div>
+    `;
+  });
+}
+
+loadStories();
